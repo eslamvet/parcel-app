fac1ad1bc0a81b283fb195c2a2f3ae18d01bea25
eslamvet1 <58374613+eslamvet@users.noreply.github.com>
2024-02-05 15:45:14 +0200

    fix some issues

--- src/app/services/parcel.service.ts
+++ src/app/services/parcel.service.ts
@@ -1,15 +1,17 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { Parcel } from '@interfaces/parcel';
 import { BehaviorSubject, EMPTY, catchError, map } from 'rxjs';
+import { BaseHrefService } from './base-href.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ParcelService {
+  BASE_HREF = inject(BaseHrefService).BASE_HREF
+  http = inject(HttpClient)
   private parcels$ = new BehaviorSubject<Parcel[]>([])
   readonly parcelsObs$ = this.parcels$.asObservable()
-  constructor(private http:HttpClient) { }
 
   getAllParcels(){
     return this.http.get<{parcels:Parcel[]}>('/assets/parcels.json').pipe(map(({parcels})=>{
